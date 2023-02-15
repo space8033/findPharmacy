d68520480d134ffbd6a172e93318fa45617ccd9d
ogayong <space8237@naver.com>
2023-02-15 15:41:56 +0900

    init : Initialize Project

---
+++ FindPharmacy/src/main/java/com/example/findpharmacy/FindPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.findpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindPharmacyApplication.class, args);
+    }
+
+}
---
+++ FindPharmacy/src/test/java/com/example/findpharmacy/FindPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.findpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindPharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
